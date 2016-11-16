b935309f14ecda3326df3e23065a7180a8bc3115
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-16 17:32:12 +0100

    delete save option

--- R/init.snowReservoir.R
+++ R/init.snowReservoir.R
@@ -7,43 +7,31 @@
 #' @param swe_h swe_h, NEED MORE EXPLANATION
 #' @param middelsca averaged sca
 #' @param snofritt snowfree, NEED MORE EXPLANATION
-#' @param SAVE Save the results, Boolean
-#' @param pathResults Path of the results. By default: $HOME
 #' @keywords snow
 #' @export
 #' @examples
 #' \dontrun{
 #' init.snowReservoir()
 #' }
-init.snowReservoir <-function(method=NULL,path=NULL,snomag=NULL,swe_h=NULL,middelsca=NULL,snofritt=NULL,SAVE=FALSE,pathResults="~/"){
+init.snowReservoir <-function(method=NULL,path=NULL,snomag=NULL,swe_h=NULL,middelsca=NULL,snofritt=NULL){
 
   snowReservoir <- switch(method,
-    "manual"    = init.snowReservoir.manual(snomag=snomag,swe_h=swe_h,middelsca=middelsca,snofritt=snofritt,SAVE=SAVE,pathResults=pathResults),
-    "load"      = init.snowReservoir.load(path=path,SAVE=SAVE,pathResults=pathResults),
+    "manual"    = init.snowReservoir.manual(snomag=snomag,swe_h=swe_h,middelsca=middelsca,snofritt=snofritt),
+    "load"      = init.snowReservoir.load(path=path),
     (message=paste0("Invalid method:", method,".")))
 
   return(snowReservoir)
 }
 
-init.snowReservoir.manual <- function(snomag,swe_h,middelsca,snofritt,SAVE,pathResults){
+init.snowReservoir.manual <- function(snomag,swe_h,middelsca,snofritt){
   snowReservoir <- list(snomag=snomag,
                swe_h=swe_h,
                middelsca=middelsca,
                snofritt=snofritt)
-  if (SAVE){
-    pathInit <- paste0(pathResults,"init/")
-    dir.create(pathInit, showWarnings = FALSE)
-    do.call("save", list(obj="snowReservoir", file=paste0(pathInit,"snowReservoir.rda")))
-  }
   return(snowReservoir)
 }
 
-init.snowReservoir.load <- function(path,SAVE,pathResults){
+init.snowReservoir.load <- function(path){
   load(paste0(path,"snowReservoir.rda"))
-  if (SAVE){
-    pathInit <- paste0(pathResults,"init/")
-    dir.create(pathInit, showWarnings = FALSE)
-    do.call("save", list(obj="snowReservoir", file=paste0(pathInit,"snowReservoir.rda")))
-  }
   return(snowReservoir)
 }
